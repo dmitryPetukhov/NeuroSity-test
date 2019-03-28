becdd98961974d254a4bbd188e6dfefe4002b507
DmitryPetukhov <47275446+dmitryPetukhov@users.noreply.github.com>
2019-03-28 12:22:08 +0300

    Add files via upload

---
+++ zadanie_7/style.js
@@ -0,0 +1,17 @@
+
+let box = document.querySelector('.box');
+let box2 = document.querySelector('.box2');
+
+box.style.background = 'red';
+box.style.height = 200+'px';
+box.style.width = 300 + 'px';
+box.style.margin = '0 auto';
+box.style.display = 'flex';
+box.style.justifyContent = 'center';
+box.style.alignItems = 'center';
+
+
+box2.style.background = 'green';
+box2.style.height = 50 +'px';
+box2.style.width = 50 + 'px';
+;
